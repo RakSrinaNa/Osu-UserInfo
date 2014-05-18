7a7b922fdb4708f464cd4bf5df5d776e44df61f3
MrCraftCod <zerderr@gmail.com>
2014-05-18 19:07:25 +0200

    Starting to place components

--- src/fr/mrcraftcod/Interface.java
+++ src/fr/mrcraftcod/Interface.java
@@ -1,7 +1,9 @@
 package fr.mrcraftcod;
 
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -9,16 +11,29 @@ import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.logging.Level;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -27,6 +42,8 @@ public class Interface extends JFrame
 	private static final long serialVersionUID = 2629819156905465351L;
 	private JFrame frame;
 	private JTextField userNameField;
+	private HashMap<String, JLabel> showingArea;
+	private ImagePanel avatar;
 
 	public static enum Mods
 	{
@@ -46,6 +63,8 @@ public class Interface extends JFrame
 
 	public Interface()
 	{
+		showingArea = new HashMap<String, JLabel>();
+		String[] fields = {"user_id", "count300", "count100", "count50", "playcount", "ranked_score", "total_score", "pp_rank", "level", "pp_raw", "accuracy", "count_rank_ss", "count_rank_s", "count_rank_a", "country"};
 		frame = new JFrame(Main.APPNAME);
 		frame.setLayout(new GridBagLayout());
 		frame.setPreferredSize(new Dimension(500, 500));
@@ -68,45 +87,224 @@ public class Interface extends JFrame
 				getInfos(userNameField.getText());
 			}
 		});
+		int avatarSize = 128;
+		avatar = new ImagePanel();
+		avatar.setMinimumSize(new Dimension(avatarSize, avatarSize));
+		avatar.setPreferredSize(new Dimension(avatarSize, avatarSize));
+		avatar.setMaximumSize(new Dimension(avatarSize, avatarSize));
+		avatar.addMouseListener(new MouseListener()
+		{
+			@Override
+			public void mouseClicked(MouseEvent arg0)
+			{}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0)
+			{}
+
+			@Override
+			public void mouseExited(MouseEvent arg0)
+			{}
+
+			@Override
+			public void mousePressed(MouseEvent arg0)
+			{
+				if(arg0.getClickCount() > 1)
+				{
+					final Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+					if(desktop != null && desktop.isSupported(Desktop.Action.BROWSE))
+						try
+						{
+							String user_id = showingArea.get("user_id").getText();
+							if(user_id.equalsIgnoreCase(""))
+								return;
+							desktop.browse(new URL("https://osu.ppy.sh/u/" + user_id).toURI());
+						}
+						catch(final Exception e)
+						{
+							Main.logger.log(Level.WARNING, "Problem when trying to open link in web browser!", e);
+						}
+				}
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0)
+			{}
+		});
 		GridBagConstraints constraint = new GridBagConstraints();
 		constraint.anchor = GridBagConstraints.PAGE_START;
 		constraint.fill = GridBagConstraints.HORIZONTAL;
+		int line = 0;
 		constraint.gridwidth = 1;
 		constraint.weightx = 0.1;
 		constraint.weighty = 1;
 		constraint.gridx = 0;
-		constraint.gridy = 0;
+		constraint.gridy = line++;
 		constraint.insets = new Insets(10, 0, 0, 0);
 		frame.add(userNameLabel, constraint);
 		constraint.insets = new Insets(3, 0, 0, 0);
-		constraint.weightx = 0.9;
+		constraint.weightx = 0.8;
 		constraint.gridx = 1;
 		frame.add(userNameField, constraint);
 		constraint.insets = new Insets(5, 0, 0, 0);
 		constraint.gridx = 2;
+		constraint.weightx = 0.1;
 		frame.add(validButon, constraint);
+		constraint.fill = GridBagConstraints.NONE;
+		constraint.gridwidth = 3;
+		constraint.insets = new Insets(5, 5, 0, 0);
+		constraint.gridx = 0;
+		constraint.gridy = line++;
+		frame.add(avatar, constraint);
+		constraint.gridy = line++;
+		JLabel tmp = new JLabel();
+		tmp.setFont(new Font(tmp.getFont().getName(), Font.PLAIN, 25));
+		Random r = new Random();
+		tmp.setForeground(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+		showingArea.put("username", tmp);
+		frame.add(tmp, constraint);
+		constraint.gridy = line++;
+		constraint.fill = GridBagConstraints.HORIZONTAL;
+		for(String param : fields)
+		{
+			constraint.gridwidth = 1;
+			constraint.gridy = line++;
+			constraint.gridx = 0;
+			constraint.weightx = 0.1;
+			frame.add(new JLabel(getText(param)), constraint);
+			constraint.gridwidth = 2;
+			constraint.gridx = 1;
+			constraint.weightx = 0.9;
+			tmp = new JLabel();
+			showingArea.put(param, tmp);
+			frame.add(tmp, constraint);
+		}
 		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation(new Point((dimension.width - 700) / 2, (dimension.height - 130) / 2));
 		frame.pack();
 		frame.toFront();
 	}
 
+	private String getText(String param)
+	{
+		return param;
+	}
+
+	private BufferedImage getAvatar(String user_id) throws MalformedURLException, IOException, Exception
+	{
+		return ImageIO.read(new URL("https:" + cutLine(getLineCodeFromLink("https://osu.ppy.sh/u/" + user_id, "<div class=\"avatar-holder\">"), true, "\" alt=\"User avatar\"", "<div class=\"avatar-holder\"><img src=\"")));
+	}
+
+	public static String cutLine(final String string, final boolean deleteDelimiters, final String ending, final String... begining) throws Exception
+	{
+		if(!string.contains(ending))
+			throw new Exception();
+		boolean exists = false;
+		for(final String temp : begining)
+			if(string.contains(temp))
+				exists = true;
+		if(!exists)
+			throw new Exception();
+		int beginingIndex = 0;
+		for(final String temp : begining)
+			if((beginingIndex = string.indexOf(temp)) > -1)
+				break;
+		String result = string.substring(beginingIndex, string.indexOf(ending) + ending.length());
+		if(deleteDelimiters)
+		{
+			result = result.replace(ending, "");
+			for(final String temp : begining)
+				result = result.replace(temp, "");
+		}
+		return result;
+	}
+
+	private static String[] getCode(String link) throws IOException
+	{
+		final URL url = new URL(link);
+		StringBuilder page = new StringBuilder();
+		String str = null;
+		final URLConnection connection = url.openConnection();
+		connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+		connection.setRequestProperty("Accept-Language", "fr-FR");
+		connection.setConnectTimeout(30000);
+		connection.setReadTimeout(30000);
+		final BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+		while((str = in.readLine()) != null)
+			page.append(str + "\n");
+		in.close();
+		return page.toString().split("\n");
+	}
+
+	public static String getLineCodeFromLink(final String link, final String... gets) throws Exception
+	{
+		final String[] lines = getCode(link);
+		for(final String get : gets)
+			for(final String tempLine : lines)
+				if(tempLine.contains(get))
+					return tempLine;
+		throw new Exception("Cannot get code from link");
+	}
+
 	private void getInfos(String user)
 	{
 		try
 		{
-			JSONObject obj = new JSONObject(getJSONText(user));
+			final JSONObject obj = new JSONObject(getJSONText(user));
+			@SuppressWarnings("unchecked")
+			Iterator<String> keys = obj.keys();
+			while(keys.hasNext())
+			{
+				String key = keys.next();
+				if(showingArea.containsKey(key))
+				{
+					showingArea.get(key).setText(obj.getString(key));
+					if(key.equals("username"))
+					{
+						Random r = new Random();
+						showingArea.get(key).setForeground(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+					}
+				}
+			}
+			SwingUtilities.invokeLater(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					try
+					{
+						avatar.setImage(getAvatar(obj.getString("user_id")));
+					}
+					catch(Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+			});
 		}
 		catch(JSONException | IOException e)
-		{}
+		{
+			e.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	private String getJSONText(String user) throws IOException
 	{
-		String urlParameters = "k=" + Main.API_KEY + "&u=" + user + "&m=" + "0" + "&type=string&event_days=1";
-		URL url = new URL(" https://osu.ppy.sh/p/api/get_user");
+		String str = null, urlParameters = "k=" + Main.API_KEY + "&u=" + user + "&m=" + "0" + "&type=string&event_days=1";
 		StringBuilder page = new StringBuilder();
-		String str = null;
+		if(true)
+		{
+			final BufferedReader in = new BufferedReader(new FileReader(new File(".", "test.json")));
+			while((str = in.readLine()) != null)
+				page.append(str);
+			in.close();
+			return page.toString();
+		}
+		URL url = new URL(" https://osu.ppy.sh/p/api/get_user");
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.setDoOutput(true);
 		connection.setDoInput(true);
