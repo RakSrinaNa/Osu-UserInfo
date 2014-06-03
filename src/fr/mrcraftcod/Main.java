854b39d15599ee982c0bc44039c79b55f73fb50d
MrCraftCod <zerderr@gmail.com>
2014-06-03 20:41:10 +0200

    New dev version (1.6b7)

--- src/fr/mrcraftcod/Main.java
+++ src/fr/mrcraftcod/Main.java
@@ -69,7 +69,7 @@ import fr.mrcraftcod.utils.Updater;
 public class Main
 {
 	public final static String APPNAME = "Osu!UserInfo";
-	public final static String VERSION = "1.6b6";
+	public final static String VERSION = "1.6b7";
 	private final static String logFileName = "log.log";
 	public static String API_KEY = "";
 	public static int numberTrackedStatsToKeep;
