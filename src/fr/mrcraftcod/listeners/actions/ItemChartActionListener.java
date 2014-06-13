fbdb6cb88b5b360798f2fa9ef64fae88400d6de1
MrCraftCod <zerderr@gmail.com>
2014-06-13 10:35:50 +0200

    Can't open graph is there isn't a user that were researched

--- src/fr/mrcraftcod/listeners/actions/ItemChartActionListener.java
+++ src/fr/mrcraftcod/listeners/actions/ItemChartActionListener.java
@@ -1,8 +1,11 @@
 package fr.mrcraftcod.listeners.actions;
 
 import java.awt.event.ActionEvent;
+import java.util.List;
 import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
 import fr.mrcraftcod.interfaces.InterfaceChart;
+import fr.mrcraftcod.objects.Stats;
 import fr.mrcraftcod.utils.Utils;
 
 public class ItemChartActionListener extends AbstractAction
@@ -12,6 +15,10 @@ public class ItemChartActionListener extends AbstractAction
 	@Override
 	public void actionPerformed(ActionEvent arg0)
 	{
-		new InterfaceChart(Utils.lastUser.getUsername(), "Osu!", Utils.lastUser.getAllStats(Utils.mainFrame.getSelectedMode()));
+		List<Stats> stats = Utils.lastUser.getAllStats(Utils.mainFrame.getSelectedMode());
+		if(stats == null || stats.size() < 1)
+			JOptionPane.showMessageDialog(Utils.mainFrame, Utils.resourceBundle.getString("select_user_chart"), Utils.resourceBundle.getString("select_user_chart_title"), JOptionPane.ERROR_MESSAGE);
+		else
+			new InterfaceChart(Utils.lastUser.getUsername(), "Osu!", stats);
 	}
 }
