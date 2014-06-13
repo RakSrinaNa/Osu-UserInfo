3cbe6b93ac512d64159d59c4bf4361d1d8c24781
MrCraftCod <zerderr@gmail.com>
2014-06-13 19:03:52 +0200

    Add ranked and total score lines

--- src/fr/mrcraftcod/interfaces/InterfaceChart.java
+++ src/fr/mrcraftcod/interfaces/InterfaceChart.java
@@ -36,7 +36,7 @@ import fr.mrcraftcod.utils.Utils;
 public class InterfaceChart extends JFrame
 {
 	private static final long serialVersionUID = -5220915498588371099L;
-	private static final Color colorPP = Color.BLUE, colorRank = Color.RED, colorAcc = Color.BLUE;
+	private static final Color colorLine1 = Color.BLUE, colorLine2 = Color.RED;
 
 	public InterfaceChart(String user, String mode, List<Stats> stats)
 	{
@@ -46,11 +46,15 @@ public class InterfaceChart extends JFrame
 		Shape shape = new Rectangle(-shapeOffset / 2, -shapeOffset / 2, shapeOffset, shapeOffset);
 		ChartPanel chartPPAndRankPanel = getChartInPannel(createRankAndPPChart(user, stats, shape));
 		ChartPanel chartAccuracyPanel = getChartInPannel(createAccuracyChart(user, stats, shape));
+		ChartPanel chartRankedScorePanel = getChartInPannel(createRankedScoreChart(user, stats, shape));
+		ChartPanel chartTotalScorePanel = getChartInPannel(createTotalScoreChart(user, stats, shape));
 		ChartPanel chartHitsPanel = getChartInPannel(createHitsChart(user, stats));
 		ChartPanel chartRanksPanel = getChartInPannel(createRanksChart(user, stats));
 		JTabbedPane contentPane = new JTabbedPane();
 		contentPane.addTab(Utils.resourceBundle.getString("rank") + " & PP", chartPPAndRankPanel);
 		contentPane.addTab(Utils.resourceBundle.getString("accuracy"), chartAccuracyPanel);
+		contentPane.addTab(Utils.resourceBundle.getString("ranked_score"), chartRankedScorePanel);
+		contentPane.addTab(Utils.resourceBundle.getString("total_score"), chartTotalScorePanel);
 		contentPane.addTab("300 / 100 / 50", chartHitsPanel);
 		contentPane.addTab("SS / S / A", chartRanksPanel);
 		setContentPane(contentPane);
@@ -66,7 +70,7 @@ public class InterfaceChart extends JFrame
 		XYPlot xyPlot = chart.getXYPlot();
 		xyPlot.setDomainAxisLocation(AxisLocation.BOTTOM_OR_LEFT);
 		NumberAxis axisAcc = new NumberAxis(Utils.resourceBundle.getString("accuracy") + " (%)");
-		axisAcc.setTickLabelPaint(colorAcc);
+		axisAcc.setTickLabelPaint(colorLine1);
 		axisAcc.setAutoRangeIncludesZero(false);
 		axisAcc.setNumberFormatOverride(new DecimalFormat("0.0000"));
 		xyPlot.setRangeAxis(0, axisAcc);
@@ -75,7 +79,7 @@ public class InterfaceChart extends JFrame
 		xyPlot.mapDatasetToRangeAxis(0, 0);
 		XYLineAndShapeRenderer rendererAccuracy = new XYLineAndShapeRenderer();
 		xyPlot.setRenderer(0, rendererAccuracy);
-		xyPlot.getRendererForDataset(xyPlot.getDataset(0)).setSeriesPaint(0, colorAcc);
+		xyPlot.getRendererForDataset(xyPlot.getDataset(0)).setSeriesPaint(0, colorLine1);
 		xyPlot.getRendererForDataset(xyPlot.getDataset(0)).setSeriesShape(0, shape);
 		DateAxis axisDate = (DateAxis) xyPlot.getDomainAxis();
 		axisDate.setDateFormatOverride(DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT));
@@ -101,15 +105,15 @@ public class InterfaceChart extends JFrame
 
 	private JFreeChart createRankAndPPChart(String user, List<Stats> stats, Shape shape)
 	{
-		JFreeChart chart = ChartFactory.createTimeSeriesChart(String.format(Utils.resourceBundle.getString("stats_for"), user), Utils.resourceBundle.getString("dates"), "%", null, true, true, false);
+		JFreeChart chart = ChartFactory.createTimeSeriesChart(String.format(Utils.resourceBundle.getString("stats_for"), user), Utils.resourceBundle.getString("dates"), "", null, true, true, false);
 		chart.setAntiAlias(true);
 		chart.setTextAntiAlias(true);
 		XYPlot xyPlot = chart.getXYPlot();
 		xyPlot.setDomainAxisLocation(AxisLocation.BOTTOM_OR_LEFT);
 		NumberAxis axisPP = new NumberAxis("PP");
 		NumberAxis axisRank = new NumberAxis(Utils.resourceBundle.getString("rank"));
-		axisPP.setTickLabelPaint(colorPP);
-		axisRank.setTickLabelPaint(colorRank);
+		axisPP.setTickLabelPaint(colorLine1);
+		axisRank.setTickLabelPaint(colorLine2);
 		axisPP.setAutoRangeIncludesZero(false);
 		axisRank.setAutoRangeIncludesZero(false);
 		axisPP.setNumberFormatOverride(new DecimalFormat("0.00"));
@@ -127,10 +131,34 @@ public class InterfaceChart extends JFrame
 		XYLineAndShapeRenderer rendererRank = new XYLineAndShapeRenderer();
 		xyPlot.setRenderer(0, rendererPP);
 		xyPlot.setRenderer(1, rendererRank);
-		xyPlot.getRendererForDataset(xyPlot.getDataset(0)).setSeriesPaint(0, colorPP);
-		xyPlot.getRendererForDataset(xyPlot.getDataset(1)).setSeriesPaint(0, colorRank);
+		xyPlot.getRendererForDataset(xyPlot.getDataset(0)).setSeriesPaint(0, colorLine1);
+		xyPlot.getRendererForDataset(xyPlot.getDataset(1)).setSeriesPaint(0, colorLine2);
 		xyPlot.getRendererForDataset(xyPlot.getDataset(0)).setSeriesShape(0, shape);
 		xyPlot.getRendererForDataset(xyPlot.getDataset(1)).setSeriesShape(0, shape);
+		DateAxis axisDate = (DateAxis) xyPlot.getDomainAxis();
+		axisDate.setDateFormatOverride(DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT));
+		return chart;
+	}
+
+	private JFreeChart createRankedScoreChart(String user, List<Stats> stats, Shape shape)
+	{
+		JFreeChart chart = ChartFactory.createTimeSeriesChart(String.format(Utils.resourceBundle.getString("stats_for"), user), Utils.resourceBundle.getString("dates"), "", null, true, true, false);
+		chart.setAntiAlias(true);
+		chart.setTextAntiAlias(true);
+		XYPlot xyPlot = chart.getXYPlot();
+		xyPlot.setDomainAxisLocation(AxisLocation.BOTTOM_OR_LEFT);
+		NumberAxis axisAcc = new NumberAxis(Utils.resourceBundle.getString("ranked_score"));
+		axisAcc.setTickLabelPaint(colorLine1);
+		axisAcc.setAutoRangeIncludesZero(false);
+		axisAcc.setNumberFormatOverride(new DecimalFormat("0"));
+		xyPlot.setRangeAxis(0, axisAcc);
+		xyPlot.setRangeAxisLocation(0, AxisLocation.TOP_OR_RIGHT);
+		xyPlot.setDataset(0, processStatsRankedScore(stats));
+		xyPlot.mapDatasetToRangeAxis(0, 0);
+		XYLineAndShapeRenderer rendererAccuracy = new XYLineAndShapeRenderer();
+		xyPlot.setRenderer(0, rendererAccuracy);
+		xyPlot.getRendererForDataset(xyPlot.getDataset(0)).setSeriesPaint(0, colorLine1);
+		xyPlot.getRendererForDataset(xyPlot.getDataset(0)).setSeriesShape(0, shape);
 		DateAxis axisDate = (DateAxis) xyPlot.getDomainAxis();
 		axisDate.setDateFormatOverride(DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT));
 		return chart;
@@ -153,6 +181,30 @@ public class InterfaceChart extends JFrame
 		return chart;
 	}
 
+	private JFreeChart createTotalScoreChart(String user, List<Stats> stats, Shape shape)
+	{
+		JFreeChart chart = ChartFactory.createTimeSeriesChart(String.format(Utils.resourceBundle.getString("stats_for"), user), Utils.resourceBundle.getString("dates"), "", null, true, true, false);
+		chart.setAntiAlias(true);
+		chart.setTextAntiAlias(true);
+		XYPlot xyPlot = chart.getXYPlot();
+		xyPlot.setDomainAxisLocation(AxisLocation.BOTTOM_OR_LEFT);
+		NumberAxis axisAcc = new NumberAxis(Utils.resourceBundle.getString("total_score"));
+		axisAcc.setTickLabelPaint(colorLine1);
+		axisAcc.setAutoRangeIncludesZero(false);
+		axisAcc.setNumberFormatOverride(new DecimalFormat("0"));
+		xyPlot.setRangeAxis(0, axisAcc);
+		xyPlot.setRangeAxisLocation(0, AxisLocation.TOP_OR_RIGHT);
+		xyPlot.setDataset(0, processStatsTotalScore(stats));
+		xyPlot.mapDatasetToRangeAxis(0, 0);
+		XYLineAndShapeRenderer rendererAccuracy = new XYLineAndShapeRenderer();
+		xyPlot.setRenderer(0, rendererAccuracy);
+		xyPlot.getRendererForDataset(xyPlot.getDataset(0)).setSeriesPaint(0, colorLine1);
+		xyPlot.getRendererForDataset(xyPlot.getDataset(0)).setSeriesShape(0, shape);
+		DateAxis axisDate = (DateAxis) xyPlot.getDomainAxis();
+		axisDate.setDateFormatOverride(DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT));
+		return chart;
+	}
+
 	private ChartPanel getChartInPannel(JFreeChart chart)
 	{
 		ChartPanel chartPanel = new ChartPanel(chart);
@@ -169,11 +221,11 @@ public class InterfaceChart extends JFrame
 
 	private XYDataset processStatsAccuracy(List<Stats> stats)
 	{
-		TimeSeries serieAcc = new TimeSeries(Utils.resourceBundle.getString("accuracy"));
+		TimeSeries serie = new TimeSeries(Utils.resourceBundle.getString("accuracy"));
 		for(Stats stat : stats)
-			serieAcc.add(new Millisecond(new Date(stat.getDate()), TimeZone.getDefault(), Locale.getDefault()), stat.getAccuracy());
+			serie.add(new Millisecond(new Date(stat.getDate()), TimeZone.getDefault(), Locale.getDefault()), stat.getAccuracy());
 		TimeSeriesCollection collection = new TimeSeriesCollection();
-		collection.addSeries(serieAcc);
+		collection.addSeries(serie);
 		return collection;
 	}
 
@@ -188,21 +240,31 @@ public class InterfaceChart extends JFrame
 
 	private XYDataset processStatsPP(List<Stats> stats)
 	{
-		TimeSeries seriePP = new TimeSeries("PP");
+		TimeSeries serie = new TimeSeries("PP");
 		for(Stats stat : stats)
-			seriePP.add(new Millisecond(new Date(stat.getDate()), TimeZone.getDefault(), Locale.getDefault()), stat.getPp());
+			serie.add(new Millisecond(new Date(stat.getDate()), TimeZone.getDefault(), Locale.getDefault()), stat.getPp());
 		TimeSeriesCollection collection = new TimeSeriesCollection();
-		collection.addSeries(seriePP);
+		collection.addSeries(serie);
 		return collection;
 	}
 
 	private XYDataset processStatsRank(List<Stats> stats)
 	{
-		TimeSeries serieRank = new TimeSeries(Utils.resourceBundle.getString("rank"));
+		TimeSeries serie = new TimeSeries(Utils.resourceBundle.getString("rank"));
 		for(Stats stat : stats)
-			serieRank.add(new Millisecond(new Date(stat.getDate()), TimeZone.getDefault(), Locale.getDefault()), stat.getRank());
+			serie.add(new Millisecond(new Date(stat.getDate()), TimeZone.getDefault(), Locale.getDefault()), stat.getRank());
 		TimeSeriesCollection collection = new TimeSeriesCollection();
-		collection.addSeries(serieRank);
+		collection.addSeries(serie);
+		return collection;
+	}
+
+	private XYDataset processStatsRankedScore(List<Stats> stats)
+	{
+		TimeSeries serie = new TimeSeries("TS");
+		for(Stats stat : stats)
+			serie.add(new Millisecond(new Date(stat.getDate()), TimeZone.getDefault(), Locale.getDefault()), stat.getRankedScore());
+		TimeSeriesCollection collection = new TimeSeriesCollection();
+		collection.addSeries(serie);
 		return collection;
 	}
 
@@ -213,5 +275,15 @@ public class InterfaceChart extends JFrame
 		data.setValue("S", stats.get(stats.size() - 1).getCountS());
 		data.setValue("A", stats.get(stats.size() - 1).getCountA());
 		return data;
+	}
+
+	private XYDataset processStatsTotalScore(List<Stats> stats)
+	{
+		TimeSeries serie = new TimeSeries("TS");
+		for(Stats stat : stats)
+			serie.add(new Millisecond(new Date(stat.getDate()), TimeZone.getDefault(), Locale.getDefault()), stat.getTotalScore());
+		TimeSeriesCollection collection = new TimeSeriesCollection();
+		collection.addSeries(serie);
+		return collection;
 	}
 }
