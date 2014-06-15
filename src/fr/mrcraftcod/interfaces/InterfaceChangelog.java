6e3d5b86cc598a1bd2418eeb38afabcecf84f410
MrCraftCod <zerderr@gmail.com>
2014-06-15 19:02:19 +0200

    Adding JFrame class for changelog frame

---
+++ src/fr/mrcraftcod/interfaces/InterfaceChangelog.java
@@ -0,0 +1,13 @@
+package fr.mrcraftcod.interfaces;
+
+import javax.swing.JFrame;
+
+public class InterfaceChangelog extends JFrame
+{
+	private static final long serialVersionUID = -8709993783125141424L;
+
+	public InterfaceChangelog()
+	{
+		super();
+	}
+}
