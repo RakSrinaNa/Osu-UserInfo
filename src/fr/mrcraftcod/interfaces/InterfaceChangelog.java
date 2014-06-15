b21b347c76897cd442acd7d65b92300e8a472e15
MrCraftCod <zerderr@gmail.com>
2014-06-15 19:21:28 +0200

    Print changelog text

--- src/fr/mrcraftcod/interfaces/InterfaceChangelog.java
+++ src/fr/mrcraftcod/interfaces/InterfaceChangelog.java
@@ -1,6 +1,10 @@
 package fr.mrcraftcod.interfaces;
 
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.WindowConstants;
+import fr.mrcraftcod.utils.Utils;
 
 public class InterfaceChangelog extends JFrame
 {
@@ -8,6 +12,23 @@ public class InterfaceChangelog extends JFrame
 
 	public InterfaceChangelog(String changelog)
 	{
-		super();
+		super("Changelog");
+		setTitle("Changelog");
+		this.setSize(400, 75);
+		setIconImages(Utils.icons);
+		setBackground(Utils.backColor);
+		getContentPane().setBackground(Utils.backColor);
+		getContentPane().setLayout(new BorderLayout());
+		JTextArea messageLabel = new JTextArea(changelog);
+		messageLabel.setBackground(Utils.backColor);
+		messageLabel.setEditable(false);
+		messageLabel.setBorder(null);
+		messageLabel.setLineWrap(true);
+		messageLabel.setWrapStyleWord(true);
+		messageLabel.setFocusable(false);
+		this.add(messageLabel, BorderLayout.CENTER);
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		setVisible(true);
+		toFront();
 	}
 }
