c68c837a4104d689875eca51de5833c03c5c7c56
MrCraftCod <zerderr@gmail.com>
2014-06-20 10:48:40 +0200

    ActionRefreshStats 'javadoc'

--- src/fr/mrcraftcod/actions/ActionRefreshStats.java
+++ src/fr/mrcraftcod/actions/ActionRefreshStats.java
@@ -4,6 +4,11 @@ import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import fr.mrcraftcod.utils.Utils;
 
+/**
+ * Used to refresh infos (on F5 press).
+ *
+ * @author MrCraftCod
+ */
 public class ActionRefreshStats extends AbstractAction
 {
 	private static final long serialVersionUID = -3422845002112474989L;
