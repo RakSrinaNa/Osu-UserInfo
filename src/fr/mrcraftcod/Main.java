d740fdca4c3b31b62f100ca0b3f462316c373a6d
MrCraftCod <zerderr@gmail.com>
2014-07-27 14:45:18 +0200

    Compiled with wrong version number, fix it

--- src/fr/mrcraftcod/Main.java
+++ src/fr/mrcraftcod/Main.java
@@ -43,7 +43,7 @@ import fr.mrcraftcod.utils.Utils;
 public class Main
 {
 	public final static String APPNAME = "Osu!UserInfo";
-	public final static String VERSION = "1.7b4";
+	public final static String VERSION = "1.7b5";
 
 	/**
 	 * Start the program.
