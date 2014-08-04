007a909abe084db7f423fbbfa0d101613d11eb5b
MrCraftCod <zerderr@gmail.com>
2014-08-04 17:30:04 +0200

    New dev version (1.8b5)

--- src/fr/mrcraftcod/Main.java
+++ src/fr/mrcraftcod/Main.java
@@ -43,7 +43,7 @@ import fr.mrcraftcod.utils.Utils;
 public class Main
 {
 	public final static String APPNAME = "Osu!UserInfo";
-	public final static String VERSION = "1.8b4";
+	public final static String VERSION = "1.8b5";
 
 	/**
 	 * Start the program.
