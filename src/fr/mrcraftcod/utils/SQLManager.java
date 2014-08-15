3cd9a1a0a86de4bef98e9378ab3c9e03430300af
MrCraftCod <zerderr@gmail.com>
2014-08-15 13:32:14 +0200

    Update creating table fail

--- src/fr/mrcraftcod/utils/SQLManager.java
+++ src/fr/mrcraftcod/utils/SQLManager.java
@@ -36,7 +36,6 @@ public class SQLManager
 
 	public int sendUpdateRequest(String request)
 	{
-		System.out.println("Sending Update\n" + request);
 		int result = 0;
 		try
 		{
@@ -47,14 +46,13 @@ public class SQLManager
 		{
 			e.printStackTrace();
 		}
-		System.out.println(result);
 		return result;
 	}
 
 	public int sendUser(User currentUser, Stats currentStats, String macString)
 	{
 		String table = currentUser.getUserID() + "_" + currentStats.getMode();
-		String request1 = "CREATE TABLE IF NOT EXISTS 2313737_0(Date VARCHAR(20), Frm VARCHAR(17), PP DECIMAL(8,3) UNSIGNED, Rank INT(9) UNSIGNED, Country_rank INT(9), Lvl DECIMAL(10,5) UNSIGNED, Ranked_score VARCHAR(36), Total_score VARCHAR(36), Total_hits VARCHAR(36), Count300 VARCHAR(36), Count100 VARCHAR(36), Count50 VARCHAR(36), Playcount INT(7) UNSIGNED, CountSS INT(7) UNSIGNED, CountS INT(7) UNSIGNED, CountA INT(7) UNSIGNED, Maximum_combo INT(5) UNSIGNED) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+		String request1 = "CREATE TABLE IF NOT EXISTS " + table + "(Date VARCHAR(20), Frm VARCHAR(17), PP DECIMAL(8,3) UNSIGNED, Rank INT(9) UNSIGNED, Country_rank INT(9), Lvl DECIMAL(10,5) UNSIGNED, Ranked_score VARCHAR(36), Total_score VARCHAR(36), Total_hits VARCHAR(36), Count300 VARCHAR(36), Count100 VARCHAR(36), Count50 VARCHAR(36), Playcount INT(7) UNSIGNED, CountSS INT(7) UNSIGNED, CountS INT(7) UNSIGNED, CountA INT(7) UNSIGNED, Maximum_combo INT(5) UNSIGNED) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 		String request2 = "INSERT INTO " + table + " SET Date='" + currentStats.getDate() + "', Frm='" + macString + "', PP='" + currentStats.getPP() + "', Rank='" + currentStats.getRank() + "', Country_rank='" + currentStats.getCountryRank() + "', Lvl='" + currentStats.getLevel() + "', Ranked_score='" + currentStats.getRankedScore() + "', Total_score='" + currentStats.getTotalScore() + "', Total_hits='" + currentStats.getTotalHits() + "', Count300='" + currentStats.getCount300() + "', Count100='" + currentStats.getCount100() + "', Count50='" + currentStats.getCount50() + "', Playcount='" + currentStats.getPlayCount() + "', CountSS='" + currentStats.getCountSS() + "', CountS='" + currentStats.getCountS() + "', CountA='" + currentStats.getCountA() + "', Maximum_combo='" + currentStats.getMaximumCombo() + "';";
 		return sendUpdateRequest(request1) + sendUpdateRequest(request2);
 	}
